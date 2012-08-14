3ac6d14943b667311d67cb455141dff606d4b0e2
Manish Gill <gill.manish90@gmail.com>
2012-08-15 01:36:24 +0530

    Refined Database Schema

--- Blog/models.py
+++ Blog/models.py
@@ -9,7 +9,7 @@ db = SQLAlchemy(app)
 What should be the ideal design of the database of the blogging engine?
     1. If it's meant to be used by more than 1 user, must store user info.
     2. A second table containing all the posts, mapping with the respective users.
-    3. The second table is the important one. What should be it's design? 
+    3. The second table is the important one. What should be it's design?
     4. Text format to store the blog posts.
     5. Associated metadata, like date and time of posting (displayed in localtime using pytz)
     6. Other stuff might be added in the future
@@ -17,12 +17,47 @@ What should be the ideal design of the database of the blogging engine?
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-    
+    username = db.Column(db.String(30), unique=True)
+    email = db.Column(db.String(20), unique=True)
+    password = db.Column(db.String(40))
 
-class Posts(db.Model):
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return '<User %r>' %self.username
+
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    post_content = db.Column(db.Text)
-    post_tags = db.Column(db.Text)
+    title = db.Column(db.String(80))
+    body = db.Column(db.Text)
     post_on = db.Column(db.DateTime(timezone=pytz.timezone('UTC')))
+    # post_on = db.Column(db.DateTime)
+
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship('Category', backref= db.backref('posts',lazy='dynamic'))
+
+    def __init__(self, title, body, category, post_on = None):
+        self.title = title
+        self.body = body
+        if post_on is None:
+            post_on = datetime.utcnow()
+        self.post_on = post_on
+        self.category = category
+
+    def __repr__(self):
+        return '<Post %r>' %self.title
+
+# Instead of having a 'tags' column in the posts, we can define categories seperately,
+# this way, we can easily query all the posts associated with a certain category.
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Category %r>' %self.name
